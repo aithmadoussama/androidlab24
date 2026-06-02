28bd15e18e1984f690d01fd08134a5053932fabb
ait hmad oussama <aithmadoussama458@gmail.com>
2026-06-02 22:09:14 +0100

    lab24

---
+++ app/src/main/java/com/example/lab24/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.lab24;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public native boolean isDebugDetected();
+    public native String helloFromJNI();
+    public native int factorial(int n);
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvStatus = findViewById(R.id.tvStatus);
+        TextView tvHello = findViewById(R.id.tvHello);
+        TextView tvFact = findViewById(R.id.tvFact);
+
+        boolean suspicious = isDebugDetected();
+
+        if (suspicious) {
+            tvStatus.setText("Etat securite : environnement suspect detecte");
+            tvStatus.setTextColor(Color.RED);
+
+            tvHello.setText("Fonction native sensible desactivee");
+            tvFact.setText("Calcul natif bloque");
+        } else {
+            tvStatus.setText("Etat securite : OK");
+            tvStatus.setTextColor(Color.parseColor("#2E7D32"));
+
+            tvHello.setText(helloFromJNI());
+
+            int result = factorial(10);
+            if (result >= 0) {
+                tvFact.setText("Factoriel de 10 = " + result);
+            } else {
+                tvFact.setText("Erreur factoriel");
+            }
+        }
+    }
+}
